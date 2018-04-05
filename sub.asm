f3f7a4791be3d03815112c2fd737d1487183aa23
DESKTOP-VOQT66P\Ahmed Munna <munnaa329@gmail.com>
2018-04-05 10:12:48 +0600

    3rd

--- sub.asm
+++ sub.asm
@@ -3,29 +3,27 @@
 .CODE
 
 MAIN PROC
-    
     MOV AH,1
+    
     INT 21H
-    
     MOV BL,AL
-    INT 21H 
     
+    INT 21H
     MOV CL,AL
     
-    ADD BL,CL
-    
-    SUB BL,48
+    SUB BL,CL
     
     MOV AH,2
+    
     MOV DL,0AH
     INT 21H
+    
     MOV DL,0DH
-    INT 21H 
-    
-    
+    INT 21H
     
     MOV DL,BL
-    INT 21H 
+    INT 21H
+    
     
     MOV AH,4CH
     INT 21H
